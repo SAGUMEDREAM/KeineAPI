3678ae7cac7f195944eb6ce5681a364eb04ea427
稀神灵梦 <touhouer@vip.qq.com>
2026-09-05 15:39:10 +0800

    更新1.0.3

--- common/src/main/java/cc/thonly/keine/mixin/FileToIdConverterAccessor.java
+++ common/src/main/java/cc/thonly/keine/mixin/FileToIdConverterAccessor.java
@@ -2,7 +2,10 @@ package cc.thonly.keine.mixin;
 
 import net.minecraft.resources.FileToIdConverter;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin({FileToIdConverter.class})
 public interface FileToIdConverterAccessor {
+    @Accessor("prefix")
+    String api$getPrefix();
 }
